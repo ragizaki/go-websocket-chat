5d07b3bf256f0db0a9f21dca7f09d1be3d168f1e
Zaki Machfj <zakimachfj@gmail.com>
2024-01-24 00:31:00 -0500

    load env variables in main file

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,23 +2,32 @@ package main
 
 import (
 	"log"
+	"server/config"
 	"server/db"
 	"server/internal/user"
 	"server/router"
 )
 
 func main() {
-	dbConnection, err := db.NewDatabase()
 
+	// instantiate database
+	dbConnection, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("Could not instantiate database: %s", err)
 	}
 	log.Printf("Database instantiated")
 
+	// load environment variables from config
+	if err := config.LoadEnv(); err != nil {
+		log.Fatalf("Could not load environment variables: %s", err)
+	}
+
+	// instantiate repository, service and handler
 	userRepository := user.NewRepository(dbConnection.GetDB())
 	userService := user.NewUserService(userRepository)
 	userHandler := user.NewHandler(userService)
 
+	// create router and start server
 	r := router.NewRouter(userHandler)
 	router.StartRouter("0.0.0.0:8080", r)
 }
