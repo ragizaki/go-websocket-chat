9a41e92d7c1f1e1baabaa501105c745d6183c091
Zaki Machfj <zakimachfj@gmail.com>
2024-01-23 23:51:58 -0500

    add unique tag to email field

--- server/db/migrations/20240124002553_add_users_table.up.sql
+++ server/db/migrations/20240124002553_add_users_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE "users" (
     "id" BIGSERIAL PRIMARY KEY,
     "username" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL
+    "email" VARCHAR(255) NOT NULL UNIQUE
 )
