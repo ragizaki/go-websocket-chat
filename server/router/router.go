400fa109ec5a9b9534a3b6d22b86480a1e91632c
Zaki Machfj <zakimachfj@gmail.com>
2024-01-24 01:01:36 -0500

    add routes for creating websocket room

--- server/router/router.go
+++ server/router/router.go
@@ -2,14 +2,21 @@ package router
 
 import (
 	"server/internal/user"
+	"server/internal/websocket"
 
 	"github.com/gin-gonic/gin"
 )
 
-func NewRouter(userHandler *user.Handler) *gin.Engine {
+func NewRouter(userHandler *user.Handler, websocketHandler *websocket.Handler) *gin.Engine {
 	router := gin.Default()
 
+	// user routes
 	router.POST("/signup", userHandler.CreateUser)
+	router.POST("/login", userHandler.Login)
+	router.GET("/logout", userHandler.Logout)
+
+	// websocket routes
+	router.POST("/ws/room", websocketHandler.CreateRoom)
 
 	return router
 }
