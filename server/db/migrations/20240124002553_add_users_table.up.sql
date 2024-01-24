1f00865858d7b0d36ee28737155949befaa5b2bf
Zaki Machfj <zakimachfj@gmail.com>
2024-01-23 19:58:07 -0500

    create migrations

--- server/db/migrations/20240124002553_add_users_table.up.sql
+++ server/db/migrations/20240124002553_add_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "users" (
     "id" BIGSERIAL PRIMARY KEY,
-    "username" VARCHAR(255) NOT NULL
+    "username" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL,
-);
+    "email" VARCHAR(255) NOT NULL
+)
