83732b179bf2e5d7d0cc5b3dbe005cb6b556a9d0
Zaki Machfj <zakimachfj@gmail.com>
2024-01-25 10:23:58 -0500

    create home page with create room form and api calls to fetch and create roomsm

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,113 +1,103 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "@/context/AuthContext";
+import { WebSocketContext } from "@/context/WebSocketContext";
+import Rooms from "@/components/Rooms";
+import { API_URL, WS_URL } from "@/constants";
+import { useRouter } from "next/navigation";
+
+export interface Room {
+  id: string;
+  name: string;
+  owner: string;
+}
 
 export default function Home() {
+  const [roomName, setRoomName] = useState("");
+  const [rooms, setRooms] = useState<Room[]>([]);
+  const { user } = useContext(AuthContext);
+  const { setConnection } = useContext(WebSocketContext);
+  const router = useRouter();
+
+  const getRooms = async () => {
+    const res = await fetch(`${API_URL}/api/ws/rooms`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const rooms = await res.json();
+    setRooms(rooms);
+  };
+
+  const createRoom = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const res = await fetch(`${API_URL}/api/ws/rooms`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ name: roomName, owner: user.username }),
+    });
+
+    setRoomName("");
+
+    if (!res.ok) {
+      if (res.status === 400) {
+        throw new Error("Room name already exists");
+      }
+      throw new Error("Unable to create room");
+    }
+
+    const newRoom = await res.json();
+    setRooms((prevRooms) => [...prevRooms, newRoom]);
+  };
+
+  const joinRoom = async (roomId: string) => {
+    const { id, username } = user;
+    const ws = new WebSocket(
+      `${WS_URL}/api/ws/rooms/${roomId}?userId=${id}&username=${username}`
+    );
+    ws.onopen = () => {
+      setConnection(ws);
+      return router.push(`/room`);
+    };
+
+    ws.onerror = (err) => {
+      alert("Could not join room, websocket error");
+      console.error(err);
+    };
+  };
+
+  useEffect(() => {
+    getRooms();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="my-8 px-4 md:mx-10 w-full h-full">
+      <form
+        className="flex justify-center my-5 p-3 space-x-3"
+        onSubmit={createRoom}
+      >
+        <input
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+          type="text"
+          className="p-3 rounded-md border-2 border-gray-300 outline-none focus:border-blue-400"
+          placeholder="Room Name"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          type="submit"
+          className="bg-blue-500 p-3 rounded-md text-white enabled:hover:bg-blue-600 transition ease-in disabled:bg-blue-200"
+          disabled={!roomName}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          Create Room
+        </button>
+      </form>
+      <Rooms rooms={rooms} joinRoom={joinRoom} />
     </main>
   );
 }
