3be84f10f7f372d2fadf3d997a00b80a77d935e4
Zaki Machfj <zakimachfj@gmail.com>
2024-01-25 10:24:48 -0500

    fix routes and add groups

--- server/router/router.go
+++ server/router/router.go
@@ -3,23 +3,40 @@ package router
 import (
 	"server/internal/user"
 	"server/internal/websocket"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter(userHandler *user.Handler, websocketHandler *websocket.Handler) *gin.Engine {
 	router := gin.Default()
 
+	// CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// define prefixes for routes (/api/auth and /api/ws)
+	api := router.Group("/api")
+	authRouter := api.Group("/auth")
+	wsRouter := api.Group("/ws")
+
 	// user routes
-	router.POST("/signup", userHandler.CreateUser)
-	router.POST("/login", userHandler.Login)
-	router.GET("/logout", userHandler.Logout)
+	authRouter.POST("/signup", userHandler.CreateUser)
+	authRouter.POST("/login", userHandler.Login)
+	authRouter.GET("/logout", userHandler.Logout)
 
 	// websocket routes
-	router.POST("/ws/rooms", websocketHandler.CreateRoom)
-	router.GET("/ws/rooms/join/:roomId", websocketHandler.JoinRoom)
-	router.GET("/ws/rooms", websocketHandler.GetRooms)
-	router.GET("/ws/clients/:roomId", websocketHandler.GetClientsInRoom)
+	wsRouter.POST("/rooms", websocketHandler.CreateRoom)
+	wsRouter.GET("/rooms/join/:roomId", websocketHandler.JoinRoom)
+	wsRouter.GET("/rooms", websocketHandler.GetRooms)
+	wsRouter.GET("/clients/:roomId", websocketHandler.GetClientsInRoom)
 
 	return router
 }
